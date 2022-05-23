2a744cff6b43625fd587e4ffcbaa8be82ee90c12
Sibasish <sahusibasish.sahu@gmail.com>
2022-05-23 20:23:51 +0530

    add fund balance route

--- App.js
+++ App.js
@@ -4,4 +4,7 @@ const port = process.env.PORT
 app.get("/",(req,res) => {
     res.send("Working... got response...")
 })
+app.get("/fundbalance",(req,res) => {
+    res.json({balance: 501})
+})
 app.listen(port,() => console.log("Listeneing..."))
