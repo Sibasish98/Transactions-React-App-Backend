117f8c83a12c74ea43a28bbe8e74e79130dae21f
Sibasish <sahusibasish.sahu@gmail.com>
2022-05-24 10:22:51 +0530

    Moved client side to server side calculation

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const bodyy = require('body-parser')
 
 app.use(cors({origin: "*"}))
+app.use(bodyy.json());
+//app.use(app.router);
+
+let balance = 0
 
 const port = process.env.PORT||5000
 app.get("/",(req,res) => {
     res.send("Working... got response...")
 })
 app.get("/fundbalance",(req,res) => {
-    res.json({balance: 501})
+    res.json({balance: balance})
 })
+
+
+const transaction = {transactions: [
+    {
+      "id": 1,
+      "type": "credit",
+      "subject": "Fund created",
+      "amount": 0,
+      "datetime": "Sun May 22 2022 06:17:00 GMT+0530 (India Standard Time)"
+    },]
+}
+
+
+app.get("/transactions",(req,res) => {
+    res.json(transaction);
+})
+
+
+app.post("/transactions",(req,res) => {
+    //console.log(req.body)
+    transaction.transactions.push(req.body)
+    if (req.body.type == "credit")
+    {
+        balance+= parseInt(req.body.amount)
+    }
+    else  if (req.body.type == "debit")
+    {
+        balance-= parseInt(req.body.amount)
+    }
+    res.send("200")
+})
+
 app.listen(port,() => console.log("Listeneing..."))
